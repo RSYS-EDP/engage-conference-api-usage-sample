76f652ff35be74266df0ead4d2999b7dd7c4e4d5
rsys-pjanakap <96531226+pjanakap@users.noreply.github.com>
2022-12-09 17:48:30 +0530

    update api added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const callRoutes = require("./routes/call");
 
 const app = express()
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // using the conference route

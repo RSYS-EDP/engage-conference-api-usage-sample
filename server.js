a0c330206c7a28201f378f823d717469a4ef1a4d
rsys-pjanakap <96531226+pjanakap@users.noreply.github.com>
2022-12-09 16:48:38 +0530

    update api added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const conferenceRoutes = require('./routes/conference')
+const callRoutes = require("./routes/call");
+
 
 const app = express()
 
@@ -9,8 +11,9 @@ app.use(cors());
 
 // using the conference route
 app.use("/accounts/", conferenceRoutes);
+app.use("/accounts/", callRoutes);
 
 // App starts listening to the defined port
-const listener = app.listen(process.env.PORT || 8000, () => {
+const listener = app.listen(process.env.PORT || 8080, () => {
     console.log('Your app is running on port ' + listener.address().port)
 })

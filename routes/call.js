3c036d260fec77a773801edf775594b10589610d
rsys-pjanakap <96531226+pjanakap@users.noreply.github.com>
2023-01-04 12:55:23 +0530

    API naming changes

--- routes/call.js
+++ routes/call.js
@@ -7,7 +7,7 @@ const callController = require("../controllers/call");
 router.post("/:ac_id/call", callController.makeCall);
 router.get("/:ac_id/call/:call_id", callController.getCallRecord);
 router.get("/:ac_id/call", callController.getMultipleCallRecords);
-router.put("/:ac_id/call/:call_id", callController.updateCall);
-router.post("/:ac_id/call/:call_id", callController.postCall);
+router.put("/:ac_id/call/:call_id", callController.updateCallPut);
+router.post("/:ac_id/call/:call_id", callController.updateCallPost);
 
 module.exports = router;

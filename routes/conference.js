e7346b5b88d6a276ec155bd351cfd9850628a7b6
rsys-pjanakap <96531226+pjanakap@users.noreply.github.com>
2023-01-05 18:35:20 +0530

    Update conference.js

--- routes/conference.js
+++ routes/conference.js
@@ -11,10 +11,5 @@ router.get('/:ac_id/room/:rm_id', conferenceController.getRoom);
 router.delete("/:ac_id/room/:rm_id", conferenceController.deleteRoom);
 router.post("/:ac_id/room/:rm_id/join", conferenceController.joinRoom);
 router.post("/:ac_id/room/:rm_id/play", conferenceController.playRoom);
-router.post("/:ac_id/call", callController.makeCall)
-router.get("/:ac_id/call/:call_id", callController.getCallRecord);
-router.get("/:ac_id/call", callController.getMultipleCallRecords);
-router.put("/:ac_id/call/:call_id", callController.updateCall)
-router.post("/:ac_id/call/:call_id", callController.postCall);
 
 module.exports = router;
